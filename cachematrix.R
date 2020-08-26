f4c8735ee667b634a1ac4983e72bc4016cddcd0f
VASAVI30 <70055101+VASAVI30@users.noreply.github.com>
2020-08-26 20:36:10 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In this program, I wrote a pair of functions, namely, "makeCacheMatrix"
+## and "cacheSolve" that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function which creates a special "matrix" object that
+## can cache its inverse for the input(which is an invertible square matrix)
 
 makeCacheMatrix <- function(x = matrix()) {
-        
-        
-
+    inv <- NULL
+      set <- function(y)
+      {
+           x <<- y
+           inv <<- NULL
+      }
+      get <- function(){x}
+      setInverse <- function(inverse) {inv <<- inverse}
+      getInverse <- function() {inv}
+      list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)     
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function which computes the inverse of the special "matrix"
+## returned by makeCacheMatrix above.if the inverse has already been calculated 
+## (and the matrix has not changed),then the cacheSolv should retreieve 
+## the inverse from the cache 
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inv <- x$getInverse()
+      if(!is.null(inv))
+      {
+             message("getting cached data")
+             return(inv)
+      }
+      mat <- x$get()
+      inv <- solve(mat, ...)
+      x$setInverse(inv)
+      inv
 }
+
+## --------------------------checking the program-----------------------------
+
+## p <- matrix(rnorm(9),3,3)
+## p1 <- makeCacheMatrix(p)
+## cacheSolve(p1) 
+
+##            [,1]       [,2]       [,3]
+## [1,]  0.1970638  0.3093324 -0.4585106
+## [2,] -0.3313558  0.1846536  0.6087729
+## [3,]  0.6750986 -0.2676764 -0.1432893
